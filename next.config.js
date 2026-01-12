aa1eac54032666106c885ecf2fa265f060255a0d
Sébastien <lopezsebastien.pro@gmail.com>
2026-01-12 22:03:14 +0100

    Fix basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'portefolio' // Remplacez par le nom exact de votre repo GitHub
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
